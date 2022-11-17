b0b66ea1af2c805607964ea15c9be9036105fd0b
GhosTHaise <Ghostrex2@gmail.com>
2022-11-17 09:42:58 +0300

    Build responsive home page

--- Client/src/assets/index.js
+++ Client/src/assets/index.js
@@ -1,9 +1,11 @@
 import close from "./close.svg"
 import menu from "./menu.svg"
 import arrowUp from "./arrow-up.svg"
+import robot from "./robot.png"
 
 export {
     close,
     menu,
-    arrowUp
+    arrowUp,
+    robot
 }
--- Client/src/components/home/Hero.jsx
+++ Client/src/components/home/Hero.jsx
@@ -1,9 +1,10 @@
 import styles from "../../style"
 import GetStarted from "./GetStarted"
+import {robot} from "../../assets"
 
 const Hero = () => {
   return (
-    <section id='home' className={`flex sm:flex-row flex-col ${styles.paddingY}`}>
+    <section id='home' className={`flex md:flex-row flex-col ${styles.paddingY}`}>
         <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
               <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
                   <img src="" alt="..." className="w-[32px] h-[32px]" />
@@ -12,16 +13,33 @@ const Hero = () => {
                         <span className="text-white">1 Mounth</span> Account
                   </p>
               </div>
-              <div className="flex flex-row justify-between items-end center w-full">
+              <div className="flex flex-row justify-between items-center w-full">
                     <h1 className="text-white flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] ss:leading-[100px] leading-[75px]">
                       The Next <br className="sm:hidden block" />{" "}<span className="text2-gradient">Generation</span>
                     </h1>
-                    <div className="md:flex hidden md:mr-4 mr-0">
+                    <div className="ss:flex hidden md:mr-4 mr-0">
                         <GetStarted />
                     </div>
-              </div>  
+              </div> 
+              <h1 className="font-poppins font-semibold text-white ss:text-[68px] text-[52px] ss:leading-[100px] leading-[75px]">
+                  Payment Method.
+              </h1>
+              <p className={`${styles.paragraph} max-w-[470px] mt-4`}>
+                  Our teams of experts uses a methodology to identify 
+                  and secure all your payments 
+                  most likely to fit all you need.
+                  We examine and accept all way of payment.
+              </p>
         </div>
-        <div className={``}></div>
+        <div className={`${styles.flexCenter} flex-[.75] flex md:my-0 my-10 relative`}>
+          <img className="h-[100%]  w-[100%] relative z-[5]" src={robot} alt="billing" />
+          <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient'></div>
+          <div className='absolute z-[1] w-[80%] h-[85%] bottom-40 white__gradient'></div>
+          <div className='absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient'></div>
+        </div>
+        <div className={` ss:hidden ${styles.flexCenter}`}>
+            <GetStarted />
+        </div>
     </section>
   )
 }
