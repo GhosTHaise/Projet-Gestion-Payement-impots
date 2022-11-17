8fcf0b96eb4b36c36eff6dd78c3de53aab3fb297
GhosTHaise <Ghostrex2@gmail.com>
2022-11-17 19:02:11 +0300

    Import assets to project

--- Client/src/assets/index.js
+++ Client/src/assets/index.js
@@ -2,10 +2,12 @@ import close from "./close.svg"
 import menu from "./menu.svg"
 import arrowUp from "./arrow-up.svg"
 import robot from "./robot.png"
+import discount from "./Discount.svg"
 
 export {
     close,
     menu,
     arrowUp,
-    robot
+    robot,
+    discount
 }
--- Client/src/components/home/Hero.jsx
+++ Client/src/components/home/Hero.jsx
@@ -1,13 +1,13 @@
 import styles from "../../style"
 import GetStarted from "./GetStarted"
-import {robot} from "../../assets"
+import {robot,discount} from "../../assets"
 
 const Hero = () => {
   return (
     <section id='home' className={`flex md:flex-row flex-col ${styles.paddingY}`}>
         <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
               <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
-                  <img src="" alt="..." className="w-[32px] h-[32px]" />
+                  <img src={discount} alt="..." className="w-[32px] h-[32px]" />
                   <p className={`${styles.paragraph} ml-2 mr-2`}>
                         <span className="text-white">20%</span> Discount for {" "}
                         <span className="text-white">1 Mounth</span> Account
