890e5c39352d1c9694b76d2ea64e93f3f83eddaa
GhosTHaise <Ghostrex2@gmail.com>
2022-11-25 23:27:21 +0300

    Finish to create all registered field

--- Client/src/components/Auth/InputField.jsx
+++ Client/src/components/Auth/InputField.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const InputField = ({type,placeholder,stateChange}) => {
+const InputField = ({type,placeholder,stateChange,adjusted}) => {
   return (
-    <input onChange={ e => stateChange(e.target.value)} placeholder={placeholder} type={type} className={`glass-effect w-full text-[16px] rounded-lg outline-none text-[white] relative px-4 py-4 mt-2`} />
+    <input onChange={ e => stateChange(e.target.value)} placeholder={placeholder} type={type} className={`glass-effect w-full  rounded-lg outline-none text-[white] relative ${(!adjusted) ? "px-4 text-[16px]" : adjusted} py-4 mt-2`} />
   )
 }
 
--- Client/src/components/Auth/Register/FragmentForm/Second.jsx
+++ Client/src/components/Auth/Register/FragmentForm/Second.jsx
@@ -10,12 +10,24 @@ const Second = () => {
     transition={{duration : .75,}}
     exit={{x : "-110%"}} 
     className={`${styles.flexStart} flex-col mt-2 absolute top-0 left-0 w-[100%]`}>
-        <label className={`${styles.paragraph} mt-4`}>Email*</label>
-        <InputField placeholder={`Exemple@gmail.com`} />
-        <label className={`${styles.paragraph} mt-4`}>Password*</label>
-        <InputField placeholder={``} />
-        <label className={`${styles.paragraph} mt-4`}>Confirm Password*</label>
-        <InputField placeholder={``} />
+        <label className={`${styles.paragraph} mt-4`}>Contry*</label>
+        <InputField placeholder={`Madagascar`} />
+        <div className='flex justify-between'>
+              <div className='flex-[1.] flex flex-col'>
+                <label className={`${styles.paragraph} mt-4`}>City*</label>
+                <InputField placeholder={`Antananarivo`} />
+              </div>
+              <div className='flex-[0.65] flex flex-col'>
+                <label className={`${styles.paragraph} mt-4`}>Zip Code*</label>
+                <InputField placeholder={``} />
+              </div>
+        </div>
+        <label className={`${styles.paragraph} mt-4`}>Phone Number*</label>
+        <div className='relative'>
+          <InputField adjusted="px-20 text-[18px] font-poppins font-semibold" placeholder={`XX XXX XX`} />
+          <h4 className='absolute top-[calc(50%+4px)] translate-y-[-50%] left-4 text-[20px] text-[#ffffffc4] font-poppins font-semibold'>+261</h4>
+        </div>
+        
     </m.div>  
   )
 }
